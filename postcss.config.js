213d11e72ceffa88f616c40c38b0c827fa8f0159
MaulanaImamMuttaqin <maulanaimamuttaqin2000@gmail.com>
2022-01-19 00:01:46 +0700

    minifiy command

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,6 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    // ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}) //pakai kalau mau buat versi minify
   },
 }
